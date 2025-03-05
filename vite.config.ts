fe50b1694069a338f28f19f8bc72c7738c686be5
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-03-05 11:44:18 -0500

    +vite SSL support
    This involved adding `@vitejs/plugin-basic-ssl` and an ENV path to the certificate. (Also switched from spaces to tabs in `package.json`)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import {defineConfig, loadEnv} from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+	plugins: [
+		basicSsl({name: 'test', certDir: loadEnv('development', process.cwd(), '').VITE_CERT_PATH}),
+		solidPlugin(),
+	],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
+})
