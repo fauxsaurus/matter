f27998e113339a8208ae52357eed644cb85c84d1
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-03-05 11:46:06 -0500

    +editable strings/numbers
    Users can now create various types of state and edit the values of strings and numbers.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
 import type {Component} from 'solid-js'
+import {createSignal, Index} from 'solid-js'
 
 import styles from './App.module.css'
 
+type IUuid = string
+type IType = 'string' | 'number' | 'object' | 'array' | 'boolean' | 'regex'
+
+const mkUuid = (): IUuid => self.crypto.randomUUID()
+
 const App: Component = () => {
+	const [state, setState] = createSignal<Record<IUuid, {id: IUuid; value: any; type: IType}>>({})
+	const uiState = () => Object.values(state()) // Needs to be an [] for `<Index>`.
+
+	const updateState = (id: IUuid, value: any, props = {}) =>
+		void setState(state =>
+			Object.assign({}, state, {[id]: {...(state[id] ?? {}), value, ...props}})
+		)
+
+	const createNewState = (type: IType) => {
+		const id = mkUuid()
+		const value = {
+			array: [],
+			boolean: false,
+			number: 0,
+			object: {},
+			regex: {pattern: '', flags: ''},
+			string: '',
+		}[type]
+		updateState(id, value, {id, type})
+	}
+
 	return (
 		<div class={styles.App}>
-			<header class={styles.header}>
-				<img src={logo} class={styles.logo} alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					class={styles.link}
-					href="https://github.com/solidjs/solid"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn Solid
-				</a>
-			</header>
+			<Index each={uiState()}>
+				{item => {
+					const {id, value, type} = item()
+
+					const input =
+						type === 'string' ? (
+							<textarea
+								oninput={event => updateState(id, event.currentTarget.value)}
+								value={value}
+							></textarea>
+						) : type === 'number' ? (
+							<input
+								type="number"
+								oninput={event =>
+									updateState(id, event.currentTarget.valueAsNumber)
+								}
+								value={value}
+							/>
+						) : (
+							`Editing type "${type}" not supported.`
+						)
+
+					return (
+						<div>
+							<label>
+								{id}: {input}
+							</label>
+						</div>
+					)
+				}}
+			</Index>
+			<div>
+				<button onClick={() => createNewState('string')}>""</button>
+				<button onClick={() => createNewState('number')}>#</button>
+				<button onClick={() => createNewState('array')}>[]</button>
+				<button onClick={() => createNewState('object')}>{'{}'}</button>
+				<button onClick={() => createNewState('boolean')}>tf</button>
+				<button onClick={() => createNewState('regex')}>*.</button>
+			</div>
+			<output>
+				<pre>{JSON.stringify(state(), null, 4)}</pre>
+			</output>
 		</div>
 	)
 }
